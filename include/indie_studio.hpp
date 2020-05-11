40e7ec45b741aaaa6af0a44534142cfe7c20f04d
Derache <Antony@localhost.localdomain>
2020-05-11 15:00:16 +0200

    [+] pushed cmakelist with libraries. Irrlicht lib is missing.

---
+++ include/indie_studio.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2019
+** indie_studio
+** File description:
+** main header
+*/
+
+#ifndef INDIE_STUDIO_H
+#define INDIE_STUDIO_H
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** indie_studio
+** File description:
+** main.c
+*/
+
+#include <SFML/Audio.h>
+#include <boost/lambda/lambda.hpp>
+#include <irrlitch.h>
+#include "indie_studio.hpp"
+
+int main(int ac, char **av)
+{
+    return 0;
+}
