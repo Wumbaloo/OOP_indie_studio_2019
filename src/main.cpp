83e058e6e8f6243c80bd0e95da107c191fb85d39
Derache <Antony@localhost.localdomain>
2020-05-11 15:46:08 +0200

    [+] fixed irrlicht -> bad name include....... and added a little prog which handle a simple window

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,37 @@
 
 #include <SFML/Audio.h>
 #include <boost/lambda/lambda.hpp>
-#include <irrlitch.h>
+#include <irrlicht.h>
 #include "indie_studio.hpp"
 
-int main(int ac, char **av)
+// using namespace irr;
+// using namespace core;
+// using namespace scene;
+// using namespace video;
+// using namespace io;
+// using namespace gui;
+
+
+int main()
 {
+    irr::IrrlichtDevice *device =
+        irr::createDevice( irr::video::EDT_SOFTWARE,
+                            irr::core::dimension2d<irr::u32>(1920, 1080), 16,
+                            false, false, false, 0);
+    if (!device)
+        return 1;
+    device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+    irr::video::IVideoDriver* driver = device->getVideoDriver();
+    irr::scene::ISceneManager* smgr = device->getSceneManager();
+    irr::gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
+    guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
+                irr::core::rect<irr::s32>(10,10,260,22), true);
+     while(device->run()) {
+        driver->beginScene(true, true, irr::video::SColor(255,100,101,140));
+        smgr->drawAll();
+        guienv->drawAll();
+        driver->endScene();
+    }
+    device->drop();
     return 0;
 }
