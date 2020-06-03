3d647520f17e7cba0df86461ddc1c896ba37622e
Nicolas Catoire <nicolas.catoire@epitech.eu>
2020-06-03 15:56:23 +0200

    changed path music

--- src/Music.cpp
+++ src/Music.cpp
@@ -8,12 +8,12 @@
 #include "Scenes.hpp"
 
 void Menu::Music_play() {
-    this->_title_music.openFromFile("../../../assets/Music/Title-music.ogg");
+    this->_title_music.openFromFile("../assets/Music/Title-music.ogg");
     this->_title_music.setLoop(true);
 }
 
 void Game::Music_play() {
-    this->_main_music.openFromFile("../../../assets/Music/Title-music.ogg");
+    this->_main_music.openFromFile("../assets/Music/Title-music.ogg");
     this->_main_music.setLoop(true);
 }
 
