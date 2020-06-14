c3aec29f2a899d7421947e6ce0d10c33fb1b3dfa
Anthony Anicotte <anthony.anicotte@epitech.eu>
2020-06-14 22:59:50 +0200

    [~] Corrected merge error on if save

--- src/Scenes/GameScene/Event.cpp
+++ src/Scenes/GameScene/Event.cpp
@@ -8,14 +8,20 @@
 #include <algorithm>
 #include "IndieStudio.hpp"
 
-bool Game::checkColision(AnimatedModel *object, std::vector<Model *> wall[], Direction dir, bool IsWallPass)
+bool Game::checkColision(AnimatedModel *object, std::vector<Model *> wall[],
+    Direction dir, bool IsWallPass
+)
 {
     for (int y = 0; y < MAP_HEIGHT + 2; y++) {
         for (Model *wall : this->_map[y]) {
             switch (dir) {
                 case UP:
-                    if ((object->getPos().Z + 2.1 >= wall->getPos().Z && object->getPos().Z < wall->getPos().Z + 1.1)
-                        && (object->getPos().X + .5 >= wall->getBoundingPos().MinEdge.X && object->getPos().X + .5 <= wall->getBoundingPos().MaxEdge.X)) {
+                    if ((object->getPos().Z + 2.1 >= wall->getPos().Z &&
+                        object->getPos().Z < wall->getPos().Z + 1.1) &&
+                        (object->getPos().X + .5 >=
+                            wall->getBoundingPos().MinEdge.X &&
+                            object->getPos().X + .5 <=
+                                wall->getBoundingPos().MaxEdge.X)) {
                         if ((IsWallPass && wall->getType() == BREAKABLE))
                             return true;
                         else
@@ -23,8 +29,12 @@ bool Game::checkColision(AnimatedModel *object, std::vector<Model *> wall[], Dir
                     }
                     break;
                 case DOWN:
-                    if ((object->getPos().Z - 2.1 <= wall->getPos().Z && object->getPos().Z > wall->getPos().Z + 1.1)
-                        && (object->getPos().X >= wall->getBoundingPos().MinEdge.X && object->getPos().X <= wall->getBoundingPos().MaxEdge.X)) {
+                    if ((object->getPos().Z - 2.1 <= wall->getPos().Z &&
+                        object->getPos().Z > wall->getPos().Z + 1.1) &&
+                        (object->getPos().X >=
+                            wall->getBoundingPos().MinEdge.X &&
+                            object->getPos().X <=
+                                wall->getBoundingPos().MaxEdge.X)) {
                         if ((IsWallPass && wall->getType() == BREAKABLE))
                             return true;
                         else
@@ -32,8 +42,12 @@ bool Game::checkColision(AnimatedModel *object, std::vector<Model *> wall[], Dir
                     }
                     break;
                 case LEFT:
-                    if ((object->getPos().X - 2.1 <= wall->getPos().X && object->getPos().X > wall->getPos().X + 1.1)
-                        && (object->getPos().Z >= wall->getBoundingPos().MinEdge.Z && object->getPos().Z <= wall->getBoundingPos().MaxEdge.Z)) {
+                    if ((object->getPos().X - 2.1 <= wall->getPos().X &&
+                        object->getPos().X > wall->getPos().X + 1.1) &&
+                        (object->getPos().Z >=
+                            wall->getBoundingPos().MinEdge.Z &&
+                            object->getPos().Z <=
+                                wall->getBoundingPos().MaxEdge.Z)) {
                         if ((IsWallPass && wall->getType() == BREAKABLE))
                             return true;
                         else
@@ -41,8 +55,12 @@ bool Game::checkColision(AnimatedModel *object, std::vector<Model *> wall[], Dir
                     }
                     break;
                 case RIGHT:
-                    if ((object->getPos().X + 2.1 >= wall->getPos().X && object->getPos().X < wall->getPos().X + 1.1)
-                        && (object->getPos().Z >= wall->getBoundingPos().MinEdge.Z && object->getPos().Z <= wall->getBoundingPos().MaxEdge.Z)) {
+                    if ((object->getPos().X + 2.1 >= wall->getPos().X &&
+                        object->getPos().X < wall->getPos().X + 1.1) &&
+                        (object->getPos().Z >=
+                            wall->getBoundingPos().MinEdge.Z &&
+                            object->getPos().Z <=
+                                wall->getBoundingPos().MaxEdge.Z)) {
                         if ((IsWallPass && wall->getType() == BREAKABLE))
                             return true;
                         else
@@ -59,7 +77,8 @@ bool Game::checkColision(AnimatedModel *object, std::vector<Model *> wall[], Dir
 
 void Game::PlayerEvents(InputManager *inputManager, IrrlichtDevice *window)
 {
-    for (auto player = this->_playerObjects.begin(); player != this->_playerObjects.end(); player++) {
+    for (auto player = this->_playerObjects.begin();
+        player != this->_playerObjects.end(); player++) {
         this->CheckPowerUpsColision((*player), window);
         this->BombHandling(window, inputManager, (*player));
         if (!(*player))
@@ -68,10 +87,12 @@ void Game::PlayerEvents(InputManager *inputManager, IrrlichtDevice *window)
             this->PlayerMovements((*player), inputManager);
         else {
             if (this->AIMovements((*player))) {
-                if (getNbBombByOwner((*player)->getName()) < (*player)->getBombUp()) {
-                    this->_bombObjects.push_back(
-                        this->createBombObject("bomb", {"bomb_animated.md3", "bomb.png",
-                        {(*player)->getPos()}, {.8, .8, .8}, {0, 20}, 20}, (*player)->getName(), window->getTimer()->getTime(), (*player)->getRange()));
+                if (getNbBombByOwner((*player)->getName()) <
+                    (*player)->getBombUp()) {
+                    this->_bombObjects.push_back(this->createBombObject("bomb",
+                        {"bomb_animated.md3", "bomb.png", {(*player)->getPos()},
+                            {.8, .8, .8}, {0, 20}, 20}, (*player)->getName(),
+                        window->getTimer()->getTime(), (*player)->getRange()));
                     this->_music->playBombSound();
                 }
             }
@@ -91,17 +112,19 @@ Events Game::PauseEvents(IrrlichtDevice *window)
             this->_music->stopSound();
             this->destroy();
             return TO_SETTINGS;
-        } else if (this->_hoverButtons[4]->isPressed())
+        } else if (this->_hoverButtons[3]->isPressed())
             return CLOSE;
     }
     return NONE;
 }
 
-Events Game::KeyboardEvents(InputManager *inputManager, IrrlichtDevice *window, settings_t *settings)
+Events Game::KeyboardEvents(InputManager *inputManager, IrrlichtDevice *window,
+    settings_t *settings
+)
 {
-    if (inputManager->isKeyPressed(SAVE_GAME)) {
     if (inputManager->isKeyPressed(SAVE_GAME) ||
-        (this->_paused && this->_hoverButtons.size() >= 4 && this->_hoverButtons[3]->isPressed())) {
+        (this->_paused && this->_hoverButtons.size() >= 4 &&
+            this->_hoverButtons[3]->isPressed())) {
         this->save(settings);
         this->_music->stopSound();
         this->destroy();
@@ -109,7 +132,7 @@ Events Game::KeyboardEvents(InputManager *inputManager, IrrlichtDevice *window, 
     }
     if (inputManager->isKeyPressed(CLOSE)) {
         this->_music->dropSound();
-        delete(this->_music);
+        delete (this->_music);
         this->destroy();
         return CLOSE;
     }
@@ -120,7 +143,8 @@ Events Game::KeyboardEvents(InputManager *inputManager, IrrlichtDevice *window, 
     if (inputManager->isKeyPressed(PAUSE))
         this->_paused = !this->_paused;
     if (inputManager->isKeyPressed(BACK_MENU) ||
-        (this->_paused && this->_hoverButtons.size() >= 2 && this->_hoverButtons[1]->isPressed())) {
+        (this->_paused && this->_hoverButtons.size() >= 2 &&
+            this->_hoverButtons[1]->isPressed())) {
         this->_music->stopSound();
         this->destroy();
         return BACK_MENU;
@@ -131,7 +155,9 @@ Events Game::KeyboardEvents(InputManager *inputManager, IrrlichtDevice *window, 
     return NONE;
 }
 
-Events Game::checkEvents(IrrlichtDevice *window, InputManager *inputManager, settings_t *settings)
+Events Game::checkEvents(IrrlichtDevice *window, InputManager *inputManager,
+    settings_t *settings
+)
 {
     const u32 now = window->getTimer()->getTime();
     Events pauseEvent = NONE;
@@ -148,5 +174,5 @@ Events Game::checkEvents(IrrlichtDevice *window, InputManager *inputManager, set
         this->_guienv->clear();
         return GO_WIN;
     }
-    return (KeyboardEvents(inputManager, window));
+    return (KeyboardEvents(inputManager, window, settings));
 }
