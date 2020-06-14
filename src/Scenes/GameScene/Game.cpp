b03151d96c6a5a9f310c2491fd301e6373b61001
Quentin Portet <quentin.portet@epitech.eu>
2020-06-14 17:02:27 +0200

    [+] pushing save/load system (testing delegated)

--- src/Scenes/GameScene/Game.cpp
+++ src/Scenes/GameScene/Game.cpp
@@ -74,8 +74,6 @@ void Game::load()
         return;
     fileStream >> buffer;
 
-    std::cout << "*** debug ***\n" << buffer << std::endl;
-
     this->makeBorderMap();
     for (int j = 0; j < MAP_HEIGHT; j++) {
         for (int i = 0; i < MAP_WIDTH; i++) {
@@ -84,17 +82,18 @@ void Game::load()
             if (buffer.at(i + MAP_WIDTH * j) == 'o') {
                 obj = this->createObject("destructible", "Cube.obj", "Cube.jpg", {(float) 0, 0, (float) 0}, {1, 1, 1}, BREAKABLE);
                 this->_map[j].push_back(obj);
-                this->placeInMap(obj, i, j);
-                std::cout << "o";
+                this->placeInMap(obj, i + 1, j + 1);
             }
             else if (buffer.at(i + MAP_WIDTH * j) == 'x') {
                 obj = this->createObject("wall", "Cube.obj", "Square.jpg", {(float) 0, 0, (float) 0}, {1, 1, 1}, OBSTACLE);
                 this->_map[j].push_back(obj);
-                this->placeInMap(obj, i, j);
-                std::cout << "x";
+                this->placeInMap(obj, i + 1, j + 1);
+            }
+            else if (buffer.at(i + MAP_WIDTH * j) == 'b') {
+                this->_powerUpObjects.push_back(this->createPowerUpObject(BOMBUP, "BombUp", {"bombUp.md3", "Rough.png",
+                {i + 1, 0, j + 1}, {.8, .8, .8}, {0, 50}, 25}));
             }
         }
-        std::cout << std::endl;
     }
     fileStream.close();
 }
